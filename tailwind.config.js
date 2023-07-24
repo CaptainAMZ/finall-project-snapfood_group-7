51087cdb0c6d006741440926f628e34832e67c64
AMZ <alirezamohammadizavele@gmail.com>
2023-07-24 22:18:11 +0330

    add: added font files and customize tailwind breakpoints and font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,27 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+
+    screens : {
+      "xs" : "320px",
+      "sm" : "600px",
+      "md" : "960px",
+      "lg" : "1280px",
+      "xs" : "1536px",
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily : {
+        "vrg" : "Vazirmatn-Regular",
+        "vmd" : "Vazirmatn-Medium",
+        "vsb" : "Vazirmatn-SemiBold",
+        "vb" : "Vazirmatn-Bold",
+        "vxb" : "Vazirmatn-ExtraBold"
+      }
     },
   },
   plugins: [],
