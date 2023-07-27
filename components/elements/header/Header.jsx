f1c68d917474743642238ecebcf9475147064296
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-07-27 12:06:50 +0330

    add (Header.jsx): add header in 320px

--- components/elements/header/Header.jsx
+++ components/elements/header/Header.jsx
@@ -2,17 +2,20 @@
 
 import React from 'react'
 import Image from 'next/image'
+import search from '../../../public/images/images-home/search.svg'
 import location from '../../../public/images/images-home/location.svg'
 import res from '../../../public/images/images-home/service-meat.png'
 import logo from '../../../public/images/images-home/logo.svg'
 import down from '../../../public/images/images-home/down.svg'
+import person from '../../../public/images/images-home/person.svg'
+import order from '../../../public/images/images-home/order.svg'
 
 export default function Header() {
   return (
     <div className='position sticky transform translate-y-0 transition all 0.3s ease-in 0s top-0 right-0 left-0 z-999 w-full'>
-        <header className='w-full bg-white shadow-header1 flex flex-col'>
-            <div className='flex-col  justify-between  items-center  p-4 h-4.5 '>
-                <div className='flex justify-end ' >
+        <header className='w-full bg-white flex flex-col'>
+            <div className=' flex justify-between  items-center p-spacing-2  max-h-spacing-9'>
+                <div className='flex-row items-center' >
 
                     <a href="" className='hidden'>
                         <Image  src={logo}
@@ -20,22 +23,23 @@ export default function Header() {
                                          height={50}
                                          alt="Picture of the author"/>       
                     </a>
-                    <div className='h-10 p-1 cursor-pointer items-center '>
+                    <div className='max-h-spacing-5 p-spacing-11 cursor-pointer  inline-flex items-center  '>
                         
-                        <Image className='ms-2.5' src={logo}
-                                            width={12}
-                                            height={14}
+                        <Image className='me-spacing-12' src={location}
+                                            width={17}
+                                            height={18}
                                             alt="location" />
                                 
                         
-                        <div>
+                        <div className='me-spacing-12 flex cursor-pointer' >
                             
-                            <div>
-                                <span > ابوذر پرستار امیر حسین معماری</span>
-                                <Image className='ms-2.5' src={down}
-                                                width={12}
-                                                height={14}
-                                                alt="location" />
+                            <div className='flex justify-center items-baseline '>
+                                <span className='pt-spacing-13 font-vrg text-vsm leading-lineHeight-tiny inline-block text-surface-overlay min-w-29 max-w-29 opacity-60 truncate w-auto ' >
+                                     ابوذر، پرستار، امیر حسین معماری</span>
+                                <Image className='ms-spacing-14 mt-spacing-11' src={down}
+                                                width={17}
+                                                height={18}
+                                                alt="down" />
 
                             </div>
                         </div>
@@ -43,6 +47,47 @@ export default function Header() {
                    
 
 
+                </div>
+
+                   
+
+                <div className='end-spacing-0 w-spacing-15  p-spacing-2 absolute left-spacing-0'>
+
+                    <Image className='ml-spacing-1' src={search}
+                                                    width={19}
+                                                    height={19}
+                                                    alt="search" />
+                        <p className='hidden'>
+                                    جست و جو در اسنپ فود
+                        </p>
+
+                </div>
+
+
+                <div className=' flex items-center z-999'> 
+
+                            <div className='p-spacing-2 cursor-pointer items-baseline flex'>
+
+                                 <Image className='ml-spacing-1 block cursor-pointer' src={person}
+                                                    width={17}
+                                                    height={18}
+                                                    alt="person" />
+                                   
+
+                                   <div>
+
+                                   </div>
+                             </div>
+
+                             <div className=' relative cursor-pointer flex max-h-spacing-6 mr-spacing-2 rounded-lgg'>
+                                            
+                                <Image className='ml-spacing-1 ' src={order}
+                                                    width={20}
+                                                    height={28}
+                                                    alt="order" />
+
+                                <p className='hidden'>سفارش ها </p>
+                             </div>
                 </div>
             </div>
      
