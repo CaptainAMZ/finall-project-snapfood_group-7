9abff5eec83f71c7e0d3f0fc70375ec2d336d3d7
mohammadamin <mostafavikhatam.mohammadamin@gmail.com>
2023-07-28 02:32:39 +0330

    add: added header of the cart without data

--- components/elements/CartResteurants.jsx
+++ components/elements/CartResteurants.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function CartResteurants() {
   return (
-    <div>CartResteurants</div>
+    <div>
+      <div>
+        <div>
+          <div>
+            <img src='#' alt='Image Restaurant'/>
+          </div>
+          <div>
+            <i>Icon</i>
+            <p>ارسال رایگان برای کاربران ویژه و.....</p>
+          </div>
+          <div>
+            <p>10%</p>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div></div>
+    </div>
   )
 }
 
