93c21b9b8c0de860c91f7a129a3b950eb0fdd0c8
mohammadamin <mostafavikhatam.mohammadamin@gmail.com>
2023-07-28 14:47:28 +0330

    add:added jsx to the service page.jsx file

--- components/elements/CartResteurants.jsx
+++ components/elements/CartResteurants.jsx
@@ -1,26 +1,9 @@
-import React from 'react'
+import React from "react";
 
 function CartResteurants() {
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <img src='#' alt='Image Restaurant'/>
-          </div>
-          <div>
-            <i>Icon</i>
-            <p>ارسال رایگان برای کاربران ویژه و.....</p>
-          </div>
-          <div>
-            <p>10%</p>
-          </div>
-        </div>
-        <div></div>
-      </div>
-      <div></div>
-    </div>
-  )
+  <div>cart</div>
+  );
 }
 
-export default CartResteurants
+export default CartResteurants;
