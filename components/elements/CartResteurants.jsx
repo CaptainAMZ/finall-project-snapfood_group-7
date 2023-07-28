f85e8f12beb3f68ece17a470efaf36578c66b530
mohammadamin <mostafavikhatam.mohammadamin@gmail.com>
2023-07-28 15:09:08 +0330

    add:added header of card Comp

--- components/elements/CartResteurants.jsx
+++ components/elements/CartResteurants.jsx
@@ -2,7 +2,45 @@ import React from "react";
 
 function CartResteurants() {
   return (
-  <div>cart</div>
+ <div>
+      <div>
+        <div>
+          <div>
+            <img src="@/public/images/rick.png" alt="rick" />
+          </div>
+          <div>
+            <i>icon</i>
+            <p>ارسال رایگان و 5%تخفیف ویژه کاربران PRO</p>
+          </div>
+          <div>
+            <p>10%</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>
+              <p>رستوران اکبر جوجه </p>
+              <div>
+                <div>
+                  <i>icon</i>
+                  <p>4.5</p>
+                  <p>(23.627 امتیاز)</p>
+                </div>
+                <div>
+                  <p>ایرانی کباب سنتی</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <i>icon</i>
+              <p>پیک فروشنده</p>
+              <p>30.000 تومان</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div></div>
+ </div>
   );
 }
 
