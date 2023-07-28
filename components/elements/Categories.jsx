3893148af4e763495a9b3128284c1d2b16a446e6
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-07-28 20:48:43 +0330

    chore(category.jsx): create category component

--- components/elements/Categories.jsx
+++ components/elements/Categories.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import Category from "@/components/elements/Category";
+import irani from "../../public/images/images-home/irani.jpg";
 
 export default function Categories() {
+  const category = [
+    { type: "ایرانی", img: irani },
+    { type: "ایرانی", img: irani },
+    { type: "ایرانی", img: irani },
+    { type: "ایرانی", img: irani },
+    { type: "ایرانی", img: irani },
+    { type: "ایرانی", img: irani },
+    { type: "ایرانی", img: irani },
+    { type: "ایرانی", img: irani },
+    { type: "ایرانی", img: irani },
+    { type: "ایرانی", img: irani },
+    { type: "ایرانی", img: irani },
+    { type: "ایرانی", img: irani },
+  ];
   return (
-    <div>
+    <div className="flex-col items-start mb-spacing-8 ">
+    <p className="font-vb font-bold leading-lineHeight-default inline-block text-carbon-light text-start mb-spacing-4 w-full ">
+      دسته بندی ها
+    </p>
 
-
-        
+    <div className="mb-spacing-4 w-[calc(100% + 2rem)] m-[calc(-1rem)] flex flex-wrap">
+      {category.map((item) => {
+        return (
+         <Category type={item.type} img={item.img}/>
+        );
+      })}
     </div>
+  </div>
   )
 }
--- components/elements/Category.jsx
+++ components/elements/Category.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 import left from '../../public/images/images-home/left.svg'
 import Image from "next/image";
 
-export default function Category() {
+export default function Category({type,img}) {
   return (
-    <div>
+    <div className="basis-1/2 max-w-1/2  p-[calc(1rem)] sm:basis-1/3 sm:max-w-1/3 md:basis-1/4 md:max-w-1/4 lg:basis-1/6 lg:max-w-1/6 " >
+    <a className="bg-transparent">
+      <div className="relative p-spacing-20 border border-solid border-white rounded-xl shadow-category cursor-pointer transition duration-200 flex bg-white ">
+        <Image src={img} className="border border-solid border-white rounded-xl w-full min-h-full object-fill"/>
+        <div className="absolute right-spacing-0 bottom-spacing-0 py-spacing-11 pr-spacing-2 pl-spacing-19 rounded-tl-xl rounded-br-xl bg-white flex items-center cursor-pointer">
+           <p className="ml-spacing-17 font-vrg text-1 leading-lineHeight-default text-carbon-main inline-block text-start cursor-pointer">
+              {type}
 
-         <a href="">
-            <div>
-                {/* <Image className='mb-spacing-2  ' src={left} width={40} height={40} alt="food" /> */}
-                <div className='absolute right-spacing-0 bottom-spacing-0  p'>
-                    <p className='font-vrg leading-lineHeight-default inline-block text-carbon-main text-start'>
-                            ایرانی
-                    </p>
-                    <Image className='mb-spacing-2  ' src={left} width={6.58} height={11.17} alt="left" />
-                </div>
-            </div>
-         </a>
-    </div>
+           </p>
+
+           <Image src={left} width={17} height={20
+           } className="translate-x-spacing-0 transition ease-in-out duration-200 cursor-pointer "/>
+        </div>
+      </div>
+    </a>
+  </div>
   )
 }
