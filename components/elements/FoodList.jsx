97c89747474cfa272ab366d4d8422fea55456d2c
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2023-07-29 22:58:26 +0330

    add: change tailwind config

--- components/elements/FoodList.jsx
+++ components/elements/FoodList.jsx
@@ -5,7 +5,7 @@ export default function  () {
   return (
     <div className='border-t-borders-xs border-r-borders-xs border-b-borders-xs border-black-alphaMedium rounded-md overflow-hidden w-full bg-surface-light flex flex-col justify-start md:px-spacing-3 '> 
       <h1 className='font-vmd text-scales-body py-spacing-2 text-surface-overlay border-l-borders-xs border-black-alphaMedium w-[100%]' > تخفیف‌ دارها</h1>
-      <div className='flex flex-col md:flex-row'>
+      <div className='flex flex-col md:flex-row flex-wrap'>
       <FoodCard/>
       <FoodCard/>
       <FoodCard/>
--- components/elements/RestaurantInfo.jsx
+++ components/elements/RestaurantInfo.jsx
@@ -4,22 +4,19 @@ import FoodList from './FoodList'
 export default function RestaurantInfo() {
   return (
     <div className='w-full min-h-screen pt-[68px]  bg-surface-main lg:max-w-[1440px] '>
-    <div className='flex flex-col  md:flex-row items-center md:items-start md:justify-center gap-spacing-10'>
+    <div className='flex flex-col  md:flex-row items-center md:items-start md:justify-center gap-spacing-3'>
 
         <div className='md:w-[25%] w-[100%]'>
             {/* Retaurant info Title section */}
             <InfoTitle/>
         </div>
         <div className='md:max-w-[100%] text-center px-spacing-3 max-w-[50rem] flex flex-col justify-center mt-spacing-3 
-        md:flex-row  bg-accent2-light gap-spacing-4'>
+        md:flex-row '>
             {/* Retaurant info Food List section */}
            <div className='md:max-w-[700px]  '>
            <FoodList/>
            </div>
-        <div className=' bg-accent-light md:max-w-[100%]'>
-           <div className='block'>
-dedeeeeeeeeeeeeeeeeeeeeeedfdfdfdfd
-           </div>
+        <div className=' bg-accent-light md:max-w-[100%] '>
         </div>
         </div>
         
