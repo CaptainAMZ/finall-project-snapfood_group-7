5d1f96988ca1fece5f85a4e9aa1f19a661db040e
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-07-30 08:26:53 +0330

    add: create Modal-Order

---
+++ components/elements/ModalOrder.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+
+export default function ModalOrder() {
+  return (
+    <div id='prev-order-modal'>
+
+            <div className='min-w-[25rem] overflow-hidden max-h-90 bg-white'>
+                <div>
+                  <button>
+
+                  </button>
+
+                  <p>
+
+                  </p>
+                </div>
+
+                <div>
+                  <span>
+
+                  </span>
+
+                  <p>
+
+
+                  </p>
+
+                  <div>
+
+                    <div>
+                      <span>
+
+                      </span>
+
+                      <div>
+                        <div>
+                          <div>
+                            <div>
+                              <span>
+
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+
+                        <Imag/>
+                        <span>
+
+                        </span>
+                      </div>
+                    </div>
+
+                  </div>
+
+                  <div>
+                    <span>
+
+                    </span>
+
+                    <div>
+                          <div>
+                            <div>
+                              <span>
+
+                              </span>
+
+                              <span>
+
+                              </span>
+                            </div>
+                          </div>
+                    </div>
+
+
+
+                  </div>
+
+                    <div>
+                    
+                    </div>
+
+                    <div>
+                    
+                    </div>
+                </div>
+            </div>
+    </div>
+  )
+}
