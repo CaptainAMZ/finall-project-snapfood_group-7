0fd627d11e92af16e996bc8ed8c1c79672129992
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-07-31 11:21:38 +0330

    test service sectiojn

--- app/page.js
+++ app/page.js
@@ -3,13 +3,14 @@ import Header from 'components/elements/Header.jsx'
 import Orders from '@/components/elements/Orders'
 import OrderItem from '@/components/elements/OrderItem'
 import ModalOrder from '@/components/elements/ModalOrder'
+import Servic from '@/components/elements/Servic'
 
 
 export default function Home() {
   return (
    
 
-     <Orders/>
+     <Servic/>
 
    
   )
