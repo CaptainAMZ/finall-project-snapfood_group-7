4e07854b526f5774c34752b368b15b64cf1bea5a
Amir Sedaghat <asedaghattalab@gmail.com>
2023-07-31 20:29:08 +0430

    delete PreOrderModal from PageSides

--- components/elements/PageSides.jsx
+++ components/elements/PageSides.jsx
@@ -3,14 +3,12 @@ import PreOrder from './PreOrder';
 import Ventor from './Ventor';
 import NoBasket from './NoBasket';
 import Basket from './Basket';
-import PreOrderModal from './PreOrderModal';
 
 function PageSides() {
   return (
     <div className='sticky top-[5.5rem] transition ease duration-0'>
       {/* PreOrderTime */}
       {/* <PreOrder/> */}
-      <PreOrderModal/>
       {/* Delivery Cost */}
       <Ventor/>
       {/* Basket Info */}
