8ae318a921654f85a7242c8c1dedce19ca28593e
Amir Sedaghat <asedaghattalab@gmail.com>
2023-07-31 11:40:46 +0430

    fix: fix a fatal error

---
+++ components/elements/PageSides.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PreOrder from './PreOrder';
+import Ventor from './Ventor';
+import NoBasket from './NoBasket';
+import Basket from './Basket';
+
+function PageSides() {
+  return (
+    <div className='sticky top-[5.5rem] transition ease duration-0'>
+      {/* PreOrderTime */}
+      <PreOrder/>
+      {/* Delivery Cost */}
+      <Ventor/>
+      {/* Basket Info */}
+      {/* <NoBasket/> */}
+      <Basket/>
+    </div>
+  )
+}
+
+export default PageSides;
