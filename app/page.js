deaa2e78427871c9f5b5d3b14466b15a524eaab9
AMZ <alirezamohammadizavele@gmail.com>
2023-07-31 23:54:28 +0330

    fix: fix some codes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,4 @@
 import './globals.css'
-import Image from 'next/image'
-import Header from 'components/elements/Header.jsx'
-import Nav from '@/components/elements/Nav'
 import Category from '@/components/elements/Category'
 import Application from '@/components/templates/Application'
 
