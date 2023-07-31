fe8be9e6783680357826b7ab14ff43b004a6b214
Amir Sedaghat <asedaghattalab@gmail.com>
2023-07-31 18:56:52 +0430

    Add: add PreOrderModal Componet to PageSides

--- components/elements/PageSides.jsx
+++ components/elements/PageSides.jsx
@@ -3,12 +3,14 @@ import PreOrder from './PreOrder';
 import Ventor from './Ventor';
 import NoBasket from './NoBasket';
 import Basket from './Basket';
+import PreOrderModal from './PreOrderModal';
 
 function PageSides() {
   return (
     <div className='sticky top-[5.5rem] transition ease duration-0'>
       {/* PreOrderTime */}
-      <PreOrder/>
+      {/* <PreOrder/> */}
+      <PreOrderModal/>
       {/* Delivery Cost */}
       <Ventor/>
       {/* Basket Info */}
