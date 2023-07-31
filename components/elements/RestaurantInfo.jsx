aee91148cafcfecf9fd5dd2aa760c9df20545b25
Amir Sedaghat <asedaghattalab@gmail.com>
2023-07-31 11:35:12 +0430

    fix: fix a fatal error

--- components/elements/RestaurantInfo.jsx
+++ components/elements/RestaurantInfo.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import PageSides from './PageSides'
 import BasketModal from './BasketModal'
 export default function RestaurantInfo() {
   return (
-    <div className='w-full min-h-screen pt-[68px] bg-surface-dark'>
+    <div className='w-full min-h-screen pt-[68px] px-[16px] bg-surface-dark'>
         <h1 className='text-center font-bold'>RestaurantInfo page(main Page)</h1>
         <div className='flex flex-col md:justify-between md:flex-row p-[calc(1rem)]'>
             <div>
+            </div>
+            <div>
               <BasketModal/>
+            </div>
+            {/* DeliveryInfo */}
+            <div className='basis-1/4 max-w-full xs:basis-full'>
+                <PageSides/>
             </div>
         </div>
     </div>
