d353c93deaa0b9b18552bae7464dc0c05121548a
AMZ <alirezamohammadizavele@gmail.com>
2023-07-31 19:31:12 +0330

    feat: divide codes to components

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Header from 'components/elements/Header.jsx'
 import Nav from '@/components/elements/Nav'
 import Category from '@/components/elements/Category'
-import Application from '@/components/modules/Application'
+import Application from '@/components/templates/Application'
 
 
 export default function Home() {
