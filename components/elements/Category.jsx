2c6240d56123d96e665efcf6ec770d0c7c6382f4
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-08-02 08:57:46 +0330

    fix(category): height of box

--- components/elements/Category.jsx
+++ components/elements/Category.jsx
@@ -6,8 +6,8 @@ export default function Category({type,img}) {
   return (
     <div className="basis-1/2 max-w-1/2  p-[calc(1rem)] sm:basis-1/3 sm:max-w-1/3 md:basis-1/4 md:max-w-1/4 lg:basis-1/6 lg:max-w-1/6 " >
     <a className="bg-transparent">
-      <div className="relative p-[0.1875rem] h-[5.9375rem] border border-solid border-white rounded-xl shadow-category cursor-pointer transition duration-200 flex bg-white ">
-        <Image src={img} className="border border-solid border-white rounded-xl w-full min-h-full object-fill"/>
+      <div className="relative p-[0.1875rem] h-[6.5rem] border border-solid border-white rounded-xl shadow-category cursor-pointer transition duration-200 flex bg-white ">
+        <Image src={img} className="border border-solid border-white rounded-xl w-full min-h-full object-cover"/>
         <div className="absolute right-spacing-0 bottom-spacing-0 py-spacing-11 pr-spacing-2 pl-[0.8125rem] rounded-tl-xl rounded-br-xl bg-white flex items-center cursor-pointer">
            <p className="ml-[0.8125rem] font-vrg text-1 leading-lineHeight-default text-carbon-main inline-block text-start cursor-pointer">
               {type}
