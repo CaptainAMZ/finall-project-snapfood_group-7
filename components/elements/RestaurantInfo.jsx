9a544e7f74be8af32ec43f6f91d78af065937a74
Amir Sedaghat <asedaghattalab@gmail.com>
2023-08-05 00:16:50 +0430

    test 1 done

--- components/elements/RestaurantInfo.jsx
+++ components/elements/RestaurantInfo.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import InfoTitle from './InfoTitle'
 import FoodList from './FoodList'
+import PageSides from './PageSides'
 export default function RestaurantInfo() {
   return (
-    <div className='flex flex-col sm:flex-row sm:flex-wrap gap-10 sm:justify-center px-4 pt-[68px] lg:px-4 lg:mx-24  '>
-     <div className=' sm:w-[45%] md:flex-1 
-     '>
-      <div className='sticky top-0 right-0'>
-        <InfoTitle/>
+    <div className='flex flex-wrap pt-[68px] px-4 pb-4'>
+      <aside className='max-w-full basis-full p-[calc(1rem)] sm:basis-1/2 md:basis-1/4 md:max-w-[25%]'>
+        <div className='sticky top-[4rem] right-0'>
+          <InfoTitle/>
+        </div>
+      </aside>
+      <div className='max-w-full basis-full p-[calc(1rem)] sm:basis-1/2 sm:max-w-[50%]'>
+        <FoodList/>
       </div>
-     </div>
-     <div className='sm:w-[45%] md:flex-3 lg:flex-[2] '>
-       <FoodList/>
-     </div>
-     <div className='bg-green-400 md:flex-1'>
-        <p>
-         
-        </p>
-     </div>
+      <aside className='max-w-full basis-full p-[calc(1rem)]p-[calc(1rem)] md:basis-1/4 md:max-w-[25%]'>
+        <PageSides/>
+      </aside>
     </div>
-    
   )
 }
+
+// flex flex-col md:justify-between md:flex-row p-[calc(1rem)]
+// flex flex-col p-[calc(1rem)] sm:flex-row sm:flex-wrap sm:justify-between lg:mx-24
