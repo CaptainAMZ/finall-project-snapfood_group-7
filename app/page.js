d5543a48b23618832566712f5488c931695bab4a
AMZ <alirezamohammadizavele@gmail.com>
2023-08-07 16:26:17 +0330

    nothing

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Categories from "@/components/elements/Categories";
 import './globals.css'
 import Category from '@/components/elements/Category'
 import Application from '@/components/templates/Application'
+import Rests from "@/components/templates/Rests";
 
 
 export default async function Home() {
@@ -14,8 +15,9 @@ export default async function Home() {
             {/* <div></div> header + nav */}
             <main className=" grow w-full max-w-85.375 mx-auto p-spacing-2 sm:p-spacing-3 md:p-spacing-5">
 
-               <Categories />
-               <Application />
+               {/* <Categories /> */}
+               {/* <Application /> */}
+               <Rests />
 
             </main>
          </div>
