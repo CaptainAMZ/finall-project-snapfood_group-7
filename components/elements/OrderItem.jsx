51650e59bc4ef4d0696f6f3f6ce6f11f22cf51be
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-08-13 12:37:45 +0330

    fix : clean code

--- components/elements/OrderItem.jsx
+++ components/elements/OrderItem.jsx
@@ -1,94 +1,87 @@
 'use client'
 
 
-import React from "react";
+import React, { useCallback } from "react";
 import Image from "next/image";
 import { useDispatch } from "react-redux";
 import { passID } from "@/redux/orderHistorySlice";
 
 
-export default function OrderItem({setmodal,modal,info}) {
-   
+export default function OrderItem({ setmodal, modal, info }) {
 
-   const dispatch = useDispatch()
-   const handlerInfo=(id)=>{
-      dispatch(passID(id))
-     
-      setmodal(true)
-   }
 
-   const {resName,id,dateHour,dateMonth,dateMin,dateDay,logo,dateDate} = info 
-   
-  
+  const dispatch = useDispatch();
+  const handlerInfo = useCallback((id) => {
+    dispatch(passID(id))
+    setmodal(true)
+  }, [])
 
 
 
-
-
-
+  const { resName, id, dateHour, dateMonth, dateMin, dateDay, logo, dateDate } = info;
   return (
-   
-<>
-  
-     <div className="pt-spacing-16 px-spacing-0 pb-spacing-1 border-b-[0.0625rem] border-carbon-alphaLight flex justify-center ">
-      <div className="w-[calc(100%-2rem)] flex flex-col ">
-        <div className="flex  items-center">
-          <div className="w-spacing-6 max-h-spacing-6 cursor-pointer flex-col">
-            <Image className="rounded w-full max-h-full" src={logo} width={100} height={100} alt="logo" />
-          </div>
-          <div className="mr-spacing-2 flex flex-col">
-            <p className="cursor-pointer font-vxb leading-lineHeight-body text-scales-body inline-block text-carbon-main text-start">
-              {resName}
-            </p>
-            <div className="flex mt-1">
-              <p className="font-vrg text-.75 leading-lineHeight-caption text-carbon-main">
-              {dateDay} {dateDate} {dateMonth}   
+
+    <>
+
+      <div className="pt-spacing-16 px-spacing-0 pb-spacing-1 border-b-[0.0625rem] border-carbon-alphaLight flex justify-center ">
+        <div className="w-[calc(100%-2rem)] flex flex-col ">
+          <div className="flex  items-center">
+            <div className="w-spacing-6 max-h-spacing-6 cursor-pointer flex-col">
+              <Image className="rounded w-full max-h-full" src={logo} width={100} height={100} alt="logo" />
+            </div>
+            <div className="mr-spacing-2 flex flex-col">
+              <p className="cursor-pointer font-vxb leading-lineHeight-body text-scales-body inline-block text-carbon-main text-start">
+                {resName}
               </p>
-              <p className="font-vrg text-.75 leading-lineHeight-caption text-carbon-main mr-spacing-2 ">
-                {dateHour}:{dateMin}
-              </p>
+              <div className="flex mt-1">
+                <p className="font-vrg text-.75 leading-lineHeight-caption text-carbon-main">
+                  {dateDay} {dateDate} {dateMonth}
+                </p>
+                <p className="font-vrg text-.75 leading-lineHeight-caption text-carbon-main mr-spacing-2 ">
+                  {dateHour}:{dateMin}
+                </p>
+              </div>
             </div>
           </div>
+
+          <div className="h-[32px] mt-spacing-2 px-spacing-1 rounded bg-accent2-alphaLight flex justify-between items-center">
+            <p className="font-vrg text-scales-tiny leading-lineHeight-tiny text-carbon-main ">
+              نظرتان را درباره این سفارش به اشتراک بگذارید.
+            </p>
+            <p className="font-vxb text-scales-caption leading-lineHeight-caption text-accent2-main cursor-pointer ml-spacing-11">
+              ثبت نظر
+            </p>
+          </div>
+
+          <div className="mt-spacing-16 mb-spacing-1 flex justify-center">
+            <button onClick={() => handlerInfo(id)} className=" flex-1 h-[32px] ml-spacing-2 inline-flex items-center justify-center min-w-6.6875 transition-all w-auto border-0.09375 border-solid  rounded-0.375 bg-carbon-alphaLight bg-clip-padding py-spacing-17 px-spacing-18">
+              <Image src='/images/images-home/factor.svg' width={16} height={16} alt="factor" />
+              <p className="mr-spacing-1  font-vxb text-scales-body leading-lineHeight-body text-carbon-main text-start inline-block flex-0">
+                مشاهده فاکتور
+              </p>
+            </button>
+
+            <button className=" flex-1 h-[32px]  inline-flex items-center justify-center min-w-6.6875 transition-all w-auto border-0.09375 border-solid  rounded-0.375 bg-carbon-alphaLight  bg-clip-padding py-spacing-17 px-spacing-18">
+              <Image src="/images/images-home/cycle.svg" width={16} height={16} alt="cycle" />
+              <p className="mr-spacing-1  font-vxb text-scales-body  leading-lineHeight-body text-carbon-main text-start inline-block flex-0">
+                سفارش مجدد
+              </p>
+            </button>
+          </div>
         </div>
 
-        <div className="h-[32px] mt-spacing-2 px-spacing-1 rounded bg-accent2-alphaLight flex justify-between items-center">
-          <p className="font-vrg text-scales-tiny leading-lineHeight-tiny text-carbon-main ">
-            نظرتان را درباره این سفارش به اشتراک بگذارید.
-          </p>
-          <p className="font-vxb text-scales-caption leading-lineHeight-caption text-accent2-main cursor-pointer ml-spacing-11">
-            ثبت نظر
-          </p>
-        </div>
 
-        <div className="mt-spacing-16 mb-spacing-1 flex justify-center">
-        <button  onClick={()=>handlerInfo(id)}  className=" flex-1 h-[32px] ml-spacing-2 inline-flex items-center justify-center min-w-6.6875 transition-all w-auto border-0.09375 border-solid  rounded-0.375 bg-carbon-alphaLight bg-clip-padding py-spacing-17 px-spacing-18">
-          <Image src='/images/images-home/factor.svg' width={16} height={16} alt="factor" />
-          <p className="mr-spacing-1  font-vxb text-scales-body leading-lineHeight-body text-carbon-main text-start inline-block flex-0">
-            مشاهده فاکتور
-          </p>
-        </button>
-
-        <button className=" flex-1 h-[32px]  inline-flex items-center justify-center min-w-6.6875 transition-all w-auto border-0.09375 border-solid  rounded-0.375 bg-carbon-alphaLight  bg-clip-padding py-spacing-17 px-spacing-18">
-          <Image src="/images/images-home/cycle.svg" width={16} height={16} alt="cycle" />
-          <p className="mr-spacing-1  font-vxb text-scales-body  leading-lineHeight-body text-carbon-main text-start inline-block flex-0">
-            سفارش مجدد
-          </p>
-        </button>
-      </div>
       </div>
 
-     
-    </div>
 
-    
 
-  
-  </> 
-   
-    
-    
-  
-  
 
-   )
+    </>
+
+
+
+
+
+
+  )
 }
--- components/elements/StaticSider.jsx
+++ components/elements/StaticSider.jsx
@@ -5,7 +5,7 @@ function StaticSider() {
   return (
     <div className="w-full  ">
       <div className="md:flex w-full h-[110px] mb-2 flex border shadow-[rgba(58,61,66,0.06)_0px_1px_0px] flex-col  rounded-xl border-solid border-[rgba(58,61,66,0.06)] hide p-3 ">
-        <p className="w-full text-[rgb(58,61,66)] text-xs font-vl h-4 text-start mr-1">
+        <p className="w-full text-[rgb(58,61,66)] text-xs font-vrg h-4 text-start mr-1">
           کلاس قیمتی
         </p>
         <div className="h-10 flex bg-[rgba(58,61,66,0.06)] cursor-pointer items-center w-full relative  mt-4 rounded-xl">
@@ -31,10 +31,10 @@ function StaticSider() {
       <div className="w-full h-[300px] border bg-[white] shadow-[rgba(58,61,66,0.06)_0px_1px_0px,rgba(0,0,0,0.05)_0px_2px_8px_-2px] p-4 rounded-xl border-solid border-[rgba(58,61,66,0.06)]">
       <div className="w-full h-1/5 flex justify-between items-center border-b-[rgba(58,61,66,0.06)] border-b border-solid">
         <span className="text-md font-vrg text-gray-600">دارای کوپن</span>
-        <label className="label-toggle">
-          <input type="checkbox"  className="input-toggle"/>
-          <span className="slider"></span>
-        </label>
+        <input 
+            type="checkbox" 
+            className="relative appearance-none inline-block h-[24px] w-[40px] cursor-pointer rounded-full bg-[rgba(58,61,66,0.06)] transition-all after:content[``]  after:absolute after:right-[2px]  after:bottom-[2px]  after:h-[20px] after:w-[20px] after:rounded-full after:bg-slate-50 after:shadow-sm after:transition-all checked:bg-lime-600 checked:after:translate-x-[-15px]" 
+          ></input>
       </div>
       <div className="w-full h-1/5 flex justify-between items-center border-b-[rgba(58,61,66,0.06)] border-b border-solid">
         <span className="text-md font-vrg text-gray-600">دارای تخفیف</span>
