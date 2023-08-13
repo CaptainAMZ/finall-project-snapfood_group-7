bcba1de1a493efada2dc51d592a2b54c8914e100
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-08-13 19:55:37 +0330

    fix: clean codes

--- app/page.js
+++ app/page.js
@@ -1,32 +1,27 @@
-import Header from '@/components/elements/Header';
-import Nav from '@/components/elements/Nav';
-import Categories from '@/components/elements/Categories';
-import Application from '@/components/templates/Application';
+import Header from "@/components/elements/Header";
+import Nav from "@/components/elements/Nav";
+import Categories from "@/components/elements/Categories";
+import Application from "@/components/templates/Application";
 import SnapApplication from "../components/modules/SnapApplication";
 import SnapBusiness from "../components/modules/SnapBusiness";
 import Cities from "../components/modules/Cities";
 import Footer from "../components/modules/Footer";
 // import CardSlider from '@/components/elements/CardSlider';
-import './globals.css'
-import { getLocalData } from '@/lib/localdata';
+import "./globals.css";
+import { getLocalData } from "@/lib/localdata";
 
 export const metadata = {
   title: "  اسنپ فود | سفارش آنلاین غذا از تمامی",
   description: "سایت اسنپ فود",
   icons: {
-    icon: '/favicon',
+    icon: "/favicon",
   },
-}
+};
 
 export default async function Home() {
-
- 
-
-
   return (
     <div>
       <div className="sticky transform translate-y-0 transition all 0.3s ease-in 0s top-0 right-0 left-0 z-[1] w-full shadow-head">
-
         <Header />
         <Nav />
       </div>
@@ -35,26 +30,17 @@ export default async function Home() {
         <div className=" grow w-full max-w-85.375 mx-auto p-[1rem] sm:p-[1.5rem] md:p-[2.5rem] ">
           <Categories />
           {/* <CardSlider restaurants={restaurants} /> */}
-          </div>
+        </div>
 
-          <div className='grow w-full max-w-85.375 mx-auto px-[1rem] sm:px-[1.5rem] md:px-[2.5rem]'>
-          <SnapApplication  />
-          <SnapBusiness  />
+        <div className="grow w-full max-w-85.375 mx-auto px-[1rem] sm:px-[1.5rem] md:px-[2.5rem]">
+          <SnapApplication />
+          <SnapBusiness />
+        </div>
 
-          </div>
-         
-           <div className="border-t-2"></div>
+        <div className="border-t-2"></div>
 
-          
-
-          <Cities />
-         
-          
-          
-        
-        
+        <Cities />
       </div>
     </div>
   );
-
 }
--- components/modules/Footer.jsx
+++ components/modules/Footer.jsx
@@ -24,56 +24,56 @@ const Footer = () => {
   return (
 
     <>
-          <section className="bg-surface-main w-100 md:flex flex-wrap  ">
-      <div className="flex p-9  md:p-12  ">
-        <div className="flex-1 max-w-fit ml-6">
-          <img
-            className="w-20 h-20"
-            src="/images/images-home/logo.svg"
-            alt=""
-          />
-        </div>
-        <div className="flex-1 flex flex-col justify-center pt-5 md:justify-start">
-          <h3 className="text-[21px] font-vmd text-accent-main mb-1">
-            اسنپ‌فود
-          </h3>
-          <p className="text-xs font-vrg">
-            تجربه سفارش غذا، از زودفود تا اسنپ‌فود
-          </p>
-          <div className="mt-8 flex flex-wrap gap-4">
-            {icons.map((item) => (
-              <div key={item} className="w-10 h-10 rounded-full flex items-center justify-center shadow-md">
-                <img className="w-6 h-6"  src={`/images/${item}.svg`} alt={item + "icon"} />
-              </div>
-            ))}
+      <section className="bg-surface-main w-100 md:flex flex-wrap  ">
+        <div className="flex p-9  md:p-12  ">
+          <div className="flex-1 max-w-fit ml-6">
+            <img
+              className="w-20 h-20"
+              src="/images/images-home/logo.svg"
+              alt=""
+            />
+          </div>
+          <div className="flex-1 flex flex-col justify-center pt-5 md:justify-start">
+            <h3 className="text-[21px] font-vmd text-accent-main mb-1">
+              اسنپ‌فود
+            </h3>
+            <p className="text-xs font-vrg">
+              تجربه سفارش غذا، از زودفود تا اسنپ‌فود
+            </p>
+            <div className="mt-8 flex flex-wrap gap-4">
+              {icons.map((item) => (
+                <div key={item} className="w-10 h-10 rounded-full flex items-center justify-center shadow-md">
+                  <img className="w-6 h-6" src={`/images/${item}.svg`} alt={item + "icon"} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="sm:flex flex-1 lg:mr-[8rem]">
-        <StaticList arr={list1} />
-        <StaticList arr={list2} />
-      </div>
-      <div className="p-11 flex justify-end md:w-[100%]  lg:w-auto lg:pt-12">
-        <img
-          className="w-[70px] h-[70px]"
-          src="/images/enamad.png"
-          alt="enamad"
-        />
-        <img
-          className="w-[70px] h-[70px]"
-          src="/images/buisness.png"
-          alt="buisness logo"
-        />
-      </div>
-    </section>
+        <div className="sm:flex flex-1 lg:mr-[8rem]">
+          <StaticList arr={list1} />
+          <StaticList arr={list2} />
+        </div>
+        <div className="p-11 flex justify-end md:w-[100%]  lg:w-auto lg:pt-12">
+          <img
+            className="w-[70px] h-[70px]"
+            src="/images/enamad.png"
+            alt="enamad"
+          />
+          <img
+            className="w-[70px] h-[70px]"
+            src="/images/buisness.png"
+            alt="buisness logo"
+          />
+        </div>
+      </section>
 
-    <div>
-      <button className="fixed right-4 bottom-4 z-[1]  w-[3rem] h-[3rem] border-[0.09375rem] rounded-[50%] bg-accent-main flex items-center justify-center">
-                <Image src={'/images/images-home/support.svg'} width={22} height={22} alt="support"/>
-      </button>
-    </div>
+      <div>
+        <button className="fixed right-4 bottom-4 z-[1]  w-[3rem] h-[3rem] border-[0.09375rem] rounded-[50%] bg-accent-main flex items-center justify-center">
+          <Image src={'/images/images-home/support.svg'} width={22} height={22} alt="support" />
+        </button>
+      </div>
     </>
-  
+
   );
 };
 
