dd7089131fc2ac4b95bc3f2f53f7e1c3effd9522
AMZ <alirezamohammadizavele@gmail.com>
2023-08-13 22:16:37 +0330

    fix: fixed header style

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir='rtl'>
       <body>
-        <div className="sticky top-0 right-0 left-0 z-[2] w-full shadow-head">
+        <div className="sticky top-0 right-0 left-0 z-[2] w-full ">
           <Header />
         </div>
         {children}
