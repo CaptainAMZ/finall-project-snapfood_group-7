a7b186d108c562f87b2187ab1d9bf3e442989fa8
AMZ <alirezamohammadizavele@gmail.com>
2023-08-14 08:25:07 +0330

    fix: fixed provider

--- components/elements/FoodList.jsx
+++ components/elements/FoodList.jsx
@@ -3,8 +3,6 @@
 
 import React from 'react'
 import { useParams, useSearchParams } from 'next/navigation'
-import { Provider } from "react-redux";
-import { store } from "@/redux/index";
 import FoodCardWrapper from './FoodCardWrapper';
 
 export default  function FoodList ({foods, fcomments}) {
@@ -24,10 +22,7 @@ export default  function FoodList ({foods, fcomments}) {
   return (
     <div className='border-borders-xs   border-surface-dark rounded-md overflow-hidden bg-surface-light flex flex-col justify-start sx:w-[100%] lg:w-[100%] '> 
       <div className='flex flex-col md:flex-row flex-wrap '>
-      <Provider store={store}>
         <FoodCardWrapper foods={foods} fcomments={fcomments} getSearch={getSearch} url={url}/>
-      </Provider>
-  
       </div>
     </div>
   )
--- components/elements/PageSliderWrapper.jsx
+++ components/elements/PageSliderWrapper.jsx
@@ -1,25 +1,12 @@
+"use client";
 
-'use client'
+import React, { use, useState } from "react";
+import PageSides from "./PageSides";
 
-import React, { use, useState } from 'react'
-import PageSides from './PageSides'
-import { Provider } from 'react-redux';
-import { store } from '@/redux';
-
-
-export default function PageSliderWrapper({ restaurants}) {
-
-     
-    
+export default function PageSliderWrapper({ restaurants }) {
   return (
-
-        <>
-        <Provider store={store}>
-        <PageSides restaurants={restaurants}/>  
-        </Provider>
-        </>
-  )
+    <>
+      <PageSides restaurants={restaurants} />
+    </>
+  );
 }
-
-
-
